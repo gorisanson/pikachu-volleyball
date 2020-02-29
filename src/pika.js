41b5fe30b95fa936de41d5ef31c4a336ac444598
Lee Kyutae <gorisanson@gmail.com>
2020-02-29 15:50:58 +0900

    Add a function that processes player movement

--- src/pika.js
+++ src/pika.js
@@ -1,14 +1,37 @@
 /*
-*  X width: 432 = 0x1B0
-*  Y width: 304 = 0x130
-* 
-*  X position coord right-direction increasing
-*  Y position coord down-direction increasing
-* 
-*  Ball radius: 20 = 0x14
-*/
+ *  X width: 432 = 0x1B0
+ *  Y width: 304 = 0x130
+ *
+ *  X position coord right-direction increasing
+ *  Y position coord down-direction increasing
+ *
+ *  Ball radius: 20 = 0x14
+ *  Player half-width: 32 = 0x20
+ */
 
+// player on left side
+const player1 = {
+  is_player2: false, // 0xA0
+  is_computer: false, // 0xA4
+  x: 0, // 0xA8
+  y: 0, // 0xAC
+  velocity_y: 0, // 0xB0
+  status: 0, // 0xC0 // 0: normal, 1: jumping, 2: jumping_and_power_hitting, 3: diving, 4: lying_down_after_diving
+  diving_direction: 0,  // 0xB4
+  lying_down_duration_left: -1  // 0xB8
+};
 
+// player on right side
+const player2 = {
+  is_player2: true, // 0xA0
+  is_computer: false, // 0xA4
+  x: 0, // 0xA8
+  y: 0, // 0xAC
+  velocity_y: 0, // 0xB0
+  status: 0, // 0xC0 // 0: normal, 1: jumping, 2: jumping_and_power_hitting, 3: diving, 4: lying_down_after_diving
+  diving_direction: 0,  // 0xB4
+  lying_down_duration_left: -1 // 0xB8
+};
 
 const ball = {
   x: 0, // 0x30
@@ -19,12 +42,15 @@ const ball = {
   is_power_hit: false // 0x68
 };
 
-function ball_trajectory(ball) {
+// FUN_00402dc0
+function process_ball_world_collision(ball) {
   const future_ball_x = ball.x + ball.velocity_x;
   // If the center of ball would get out of left world bound or right world bound
   //
   // TODO:
-  // future_ball_x < 20 shoud be changed to future_ball_x < 0
+  // future_ball_x > 432 should be changed to future_ball_x > (432 - 20)
+  // [maybe upper one is more possible when seeing pikachu player's x-direction boundary]
+  // or, future_ball_x < 20 should be changed to future_ball_x < 0
   // I think this is a mistake of the author of the original game.
   if (future_ball_x < 20 || future_ball_x > 432) {
     ball.velocity_x = -ball.velocity_x;
@@ -51,7 +77,6 @@ function ball_trajectory(ball) {
     }
   }
 
-
   future_ball_y = ball.y + ball.velocity_y;
   // if ball would touch ground
   if (future_ball_y > 252) {
@@ -67,7 +92,8 @@ function ball_trajectory(ball) {
   return 0;
 }
 
-function ball_pika_collision(
+// FUN_004030a0
+function process_ball_pika_collision(
   ball,
   param_1,
   param_2,
@@ -118,6 +144,191 @@ function ball_pika_collision(
   }
 
   // TODO: here call function which expect landing point x of ball
-  
+  // FUN_004031b0
+
   return 1;
+}
+
+function Fun_00403dd0() {
+  let uVar4 = 0;
+  let ppvVar7 = pp_to_p1;
+  let local_20 = process_ball_world_collision(p_to_ball);
+  let local_3c = pram_1;
+  let ppvVar6 = ppvVar7;
+  let local_14 = ball;
+  let local_lc_0 = player_2;
+  let local_lc_1 = player_1;
+
+  for (let i = 0; i < 2; i++) {
+    FUN_004031b0();
+    process_player_movement(player, keyboard, the_other_player, ball);
+  }
+}
+
+// FUN_00401fc0
+// param1_array maybe keyboard (if param_1[1] === -1, up_key downed)
+// param1[0] === -1: left key downed, param1[0] === 1: right key downed.
+// param1[0] === 0: left/right key not downed.
+function process_player_movement(player, keyboard, the_other_player, ball) {
+  if (player === null || ball === null) {
+    return 0;
+  }
+  
+  if (player.is_computer === true) {
+    // maybe computer ai function?
+    FUN_00402460(player, ball, the_other_player, keyboard);
+  }
+
+  // if player is lying down..
+  if (player.status === 4) {
+    player.lying_down_duration_left += -1;
+    if (player.lying_down_duration_left < -1) {
+      player.status = 0;
+    }
+    return 1;
+  }
+  
+  // process x-direction movement
+  let player_velocity_x = 0;
+  if (player.status < 5) {
+    if (player.status < 3) {
+      if (keyboard.left_key_downed === true) {
+        player_velocity_x = -6;
+      } else if (keyboard.right_key_downed === true) {
+        player_velocity_x = 6;
+      }
+    } else {  // if player is diving..
+      player_velocity_x = player.diving_direction * 8;
+    }
+  }
+
+  let future_player_x = player.x + player_velocity_x;
+  player.x = future_player_x;
+
+  // process player's x-direction world boundary
+  if (player.is_player2 === false) {  // if player is player1
+    if (future_player_x < 32) {
+      player.x = 32;
+    } else if (future_player_x > 216 - 32) {
+      player.x = 216 - 32;
+    }
+  } else {  // if player is player2
+    if (future_player_x < 216 + 32) {
+      player.x = 216 + 32;
+    } else if (future_player_x > 432 - 32) {
+      player.x = 432 - 32;
+    }
+  }
+
+  // jump
+  // player.y == 244 means player is touching ground
+  if (player.status < 3 && keyboard.up_key_downed === true && player.y === 244) {
+    player.velocity_y = -16;
+    player.status = 1;
+    player.c4 = 0;
+    //TODO: image function
+    //TODO: sound function "chu~"
+  }
+
+  // gravity
+  let future_player_y = player.y + player.velocity_y;
+  player.y = future_player_y;
+  if (future_player_y < 244) {
+    player.velocity_y += 1;
+  } else if (future_player_y > 244) { // if player is landing..
+    player.velocity_y = 0;
+    player.y = 244;
+    player.c4 = 0;
+    if (player.status === 3) { // if player is diving..
+      player.status = 4;
+      player.c4 = 0;
+      player.lying_down_duration_left = 3;
+    } else {
+      player.status = 0;
+    }
+  }
+
+  if (keyboard.power_hit_key_pressed_this_key_should_not_auto_repeated === true) {
+    if (player.status === 1) {
+      // if player is jumping..
+      // then player do power hit!
+      iVar3 = player.x;
+      player.cc = 5;
+      player.c4 = 0;
+      player.status = 2;
+      //TODO: sound function "pik~"
+      //TODO: image function
+      //TODO: sound function "ika!"
+      piVar4 = 33333;
+    } else if (
+      player.status === 0 &&
+      (keyboard.left_key_downed === true || keyboard.right_key_downed === true)
+    ) {
+      // then player do diving!
+      player.status = 3;
+      player.c4 = 0;
+      if (left_key_downed === true) {
+        player.diving_direction = -1;
+      } else {
+        player.diving_direction = +1;
+      }
+      player.velocity_y = -5;
+      //TODO: image function
+      //TODO: sound function "chu~"
+    }
+  }
+
+  if (player.status === 1) {
+    player.c4 = (player.c4 + 1) % 3;
+  } else {
+    if (player.status === 2) {
+      if (player.cc < 1) {
+        player.c4 += 1;
+        if (player.c4 > 4) {
+          player.c4 = 0;
+          player.status = 1;
+        }
+      } else {
+        player.cc += -1;
+      }
+    } else if (player.status === 0) {
+      player.cc += 1;
+      if (player.cc > 3) {
+        player.cc = 0;
+        temp = player.c4 + player.c8;
+        if (temp < 0 || temp > 4) {
+          player.c8 = -player.c8;
+        }
+        player.c4 = player.c4 + player.c8;
+      }
+    }
+  }
+  if (player.d4 === 1) {
+    if (player.status === 0) {
+      if (player.d0 === 1) {
+        player.status = 5;
+        //TODO: image function
+        //TODO: sound function "what?"
+      } else {
+        player.status === 6;
+      }
+      player.cc = 0;
+      player.c4 = 0;
+    }
+    FUN_004025e0(player);
+  }
+  return 1;
+}
+
+function FUN_004025e0(player) {
+  let iVar1;
+  if (player.d4 !== 0 && player.c4 < 4) {
+    player.cc += 1;
+    if (player.cc > 4) {
+      player.cc = 0;
+      player.c4 += 1;
+    }
+    return 1;
+  }
+  return 0;
 }
