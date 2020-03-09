15fdec45d952bde7581257c76e8275734118eb44
Lee, Kyutae <gorisanson@gmail.com>
2020-03-09 21:37:11 +0900

    Fix webpack entry point
    one entry point "./src/main.js" is only needed

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,18 +7,9 @@ module.exports = {
   devServer: {
     contentBase: "./dist"
   },
-  entry: {
-    main: "./src/main.js",
-    pikavolley: "./src/pikavolley.js",
-    pika_view: "./src/pika_view.js",
-    pika_physics: "./src/pika_physics.js",
-    pika_cloud_and_wave: "./src/pika_cloud_and_wave.js",
-    pika_audio: "./src/pika_audio.js",
-    pika_keyboard: "./src/pika_keyboard.js",
-    rand: "./src/rand.js"
-  },
+  entry: "./src/main.js",
   output: {
-    filename: "[name].bundle.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
   plugins: [new CopyPlugin([{ from: "src/assets", to: "dist/assets" }])]
--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,10 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  //mode: "production",
-  entry: {
-    main: "./src/main.js",
-    pikavolley: "./src/pikavolley.js",
-    pika_view: "./src/pika_view.js",
-    pika_physics: "./src/pika_physics.js",
-    pika_cloud_and_wave: "./src/pika_cloud_and_wave.js",
-    pika_audio: "./src/pika_audio.js",
-    pika_keyboard: "./src/pika_keyboard.js",
-    rand: "./src/rand.js"
-  },
+  mode: "production",
+  entry: "./src/main.js",
   output: {
-    filename: "[name].bundle.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
   plugins: [
