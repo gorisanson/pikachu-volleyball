86eadf343bd083580d296f734fcbf3d9496e7bd9
Lee, Kyutae <gorisanson@gmail.com>
2020-03-10 21:46:44 +0900

    Change to single quote

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,22 +1,22 @@
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: "./dist"
+    contentBase: './dist'
   },
-  entry: { main: "./src/main.js" },
+  entry: { main: './src/main.js' },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new CopyPlugin([{ from: "src/assets", to: "dist/assets" }]),
+    new CopyPlugin([{ from: 'src/assets', to: 'dist/assets' }]),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
       hash: true
     })
   ]
