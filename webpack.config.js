48fa0e41abe3a8132d0448105d4018da27df3bfe
Lee, Kyutae <gorisanson@gmail.com>
2020-03-10 21:46:44 +0900

    Change to single quote

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  entry: { main: "./src/main.js" },
+  mode: 'production',
+  entry: { main: './src/main.js' },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new CleanWebpackPlugin(),
     new CopyPlugin([
-      { from: "src/assets", to: "assets" },
-      { from: "src/index.html" }
+      { from: 'src/assets', to: 'assets' },
+      { from: 'src/index.html' }
     ]),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
       hash: true
     })
   ]
