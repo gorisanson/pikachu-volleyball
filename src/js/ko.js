6d0b51b4e958e565d1e305ec9ee5e8bdfea6f2d3
Lee, Kyutae <gorisanson@gmail.com>
2020-03-22 23:39:00 +0900

    Add ko.js for message sprites translated to Korean

---
+++ src/js/ko.js
@@ -0,0 +1,15 @@
+/*
+ * Execute this script to use message sprites translated to Korean.
+ */
+
+import { ASSETS_PATH } from './assets_path.js';
+
+const TEXTURES = ASSETS_PATH.TEXTURES;
+
+TEXTURES.MARK = 'messages/ko/mark.png';
+TEXTURES.POKEMON = 'messages/ko/pokemon.png';
+TEXTURES.PIKACHU_VOLLEYBALL = 'messages/ko/pikachu_volleyball.png';
+TEXTURES.FIGHT = 'messages/ko/fight.png';
+TEXTURES.WITH_COMPUTER = 'messages/ko/with_computer.png';
+TEXTURES.WITH_FRIEND = 'messages/ko/with_friend.png';
+TEXTURES.GAME_START = 'messages/ko/game_start.png';
