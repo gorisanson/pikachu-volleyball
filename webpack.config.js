3a154fd7ac237bada46b80a2b058d9917e3a44d8
Lee, Kyutae <gorisanson@gmail.com>
2020-03-22 20:35:30 +0900

    Rearrage JavaScript files

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: { main: './src/main.js' },
+  entry: { main: './src/js/main.js' },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
