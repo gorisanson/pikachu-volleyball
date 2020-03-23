262d9f7405ccd8658cc1e72dcef86fe4e5a65f5d
Lee, Kyutae <gorisanson@gmail.com>
2020-03-23 19:17:47 +0900

    Add Korean version and modify others accordingly

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,19 +8,33 @@ module.exports = {
   devServer: {
     contentBase: './dist'
   },
-  entry: { main: './src/js/main.js' },
+  entry: { main: './src/resources/js/main.js', ko: './src/resources/js/ko.js' },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  optimization: {
+    runtimeChunk: { name: 'runtime' } // this is for code-sharing between "main.js and "ko.js"
+  },
   plugins: [
     new CopyPlugin([
-      { from: 'src/assets', to: 'dist/assets' },
-      { from: 'src/style.css' }
+      { from: 'src/resources/assets/', to: 'resources/assets/' },
+      { from: 'src/resources/style.css', to: 'resources/style.css' },
+      { from: 'src/index.html', to: 'index.html' }
     ]),
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      hash: true
+      template: 'src/en/index.html',
+      filename: 'en/index.html',
+      hash: true,
+      chunks: ['main', 'runtime'],
+      chunksSortMode: 'manual'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/ko/index.html',
+      filename: 'ko/index.html',
+      hash: true,
+      chunks: ['ko', 'main', 'runtime'],
+      chunksSortMode: 'manual'
     })
   ]
 };
--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,34 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: { main: './src/js/main.js' },
+  entry: { main: './src/resources/js/main.js', ko: './src/resources/js/ko.js' },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  optimization: {
+    runtimeChunk: { name: 'runtime' } // this is for code-sharing between "main.js and "ko.js"
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new CopyPlugin([
-      { from: 'src/assets', to: 'assets' },
-      { from: 'src/style.css' }
+      { from: 'src/resources/assets/', to: 'resources/assets/' },
+      { from: 'src/resources/style.css', to: 'resources/style.css' },
+      { from: 'src/index.html', to: 'index.html' }
     ]),
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      hash: true
+      template: 'src/en/index.html',
+      filename: 'en/index.html',
+      hash: true,
+      chunks: ['main', 'runtime'],
+      chunksSortMode: 'manual'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/ko/index.html',
+      filename: 'ko/index.html',
+      hash: true,
+      chunks: ['ko', 'main', 'runtime'],
+      chunksSortMode: 'manual'
     })
   ]
 };
