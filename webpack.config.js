2068e65ac9874f2030b2406d7b58795277d59b62
Lee, Kyutae <gorisanson@gmail.com>
2020-03-26 14:46:30 +0900

    Add service worker

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: { main: './src/resources/js/main.js', ko: './src/resources/js/ko.js' },
   output: {
     filename: '[name].bundle.js',
@@ -16,23 +17,31 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new CopyPlugin([
-      { from: 'src/resources/assets/', to: 'resources/assets/' },
+      {
+        context: 'src/',
+        from: 'resources/assets/**/*.+(json|png|mp3|wav)'
+      },
+      { from: 'src/en/manifest.json', to: 'en/manifest.json' },
+      { from: 'src/ko/manifest.json', to: 'ko/manifest.json' },
       { from: 'src/resources/style.css', to: 'resources/style.css' },
       { from: 'src/index.html', to: 'index.html' }
     ]),
     new HtmlWebpackPlugin({
       template: 'src/en/index.html',
       filename: 'en/index.html',
-      hash: true,
       chunks: ['runtime', 'main'],
       chunksSortMode: 'manual'
     }),
     new HtmlWebpackPlugin({
       template: 'src/ko/index.html',
       filename: 'ko/index.html',
-      hash: true,
       chunks: ['runtime', 'ko', 'main'],
       chunksSortMode: 'manual'
+    }),
+    new WorkboxPlugin.GenerateSW({
+      swDest: 'sw.js',
+      cleanupOutdatedCaches: true,
+      skipWaiting: false
     })
   ]
 };
