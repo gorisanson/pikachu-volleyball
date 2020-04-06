66aa4f57b32fae37e1bf28f4cb4ecb0b9f632524
Lee, Kyutae <gorisanson@gmail.com>
2020-04-06 20:12:52 +0900

    Refactor webpack configuration files by ...
    seperating common configuration.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,58 +1,6 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'production',
-  entry: { main: './src/resources/js/main.js', ko: './src/ko/ko.js' },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    runtimeChunk: { name: 'runtime' }, // this is for code-sharing between "main.js and "ko.js"
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new CopyPlugin([
-      {
-        context: 'src/',
-        from: 'resources/assets/**/*.+(json|png|mp3|wav)',
-      },
-      { from: 'src/en/manifest.json', to: 'en/manifest.json' },
-      { from: 'src/ko/manifest.json', to: 'ko/manifest.json' },
-      { from: 'src/resources/style.css', to: 'resources/style.css' },
-      { from: 'src/index.html', to: 'index.html' },
-    ]),
-    new HtmlWebpackPlugin({
-      template: 'src/en/index.html',
-      filename: 'en/index.html',
-      chunks: ['runtime', 'main'],
-      chunksSortMode: 'manual',
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/ko/index.html',
-      filename: 'ko/index.html',
-      chunks: ['runtime', 'ko', 'main'],
-      chunksSortMode: 'manual',
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-      },
-    }),
-    new WorkboxPlugin.GenerateSW({
-      swDest: 'sw.js',
-      cleanupOutdatedCaches: true,
-      skipWaiting: false,
-    }),
-  ],
-};
+});
