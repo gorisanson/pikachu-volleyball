562b46d7fed7e2eeefb30c0601142f03891bf5f9
Lee, Kyutae <gorisanson@gmail.com>
2021-09-25 10:29:29 +0900

    Update close button in About box
    to initially have text "Play".

--- src/resources/js/main.js
+++ src/resources/js/main.js
@@ -95,6 +95,12 @@ function setUpInitialUI() {
     }
     aboutBtn.getElementsByClassName('text-play')[0].classList.add('hidden');
     aboutBtn.getElementsByClassName('text-about')[0].classList.remove('hidden');
+    closeAboutBtn
+      .getElementsByClassName('text-play')[0]
+      .classList.add('hidden');
+    closeAboutBtn
+      .getElementsByClassName('text-close')[0]
+      .classList.remove('hidden');
 
     loader.load(setup); // setup is called after loader finishes loading
     loadingBox.classList.remove('hidden');
