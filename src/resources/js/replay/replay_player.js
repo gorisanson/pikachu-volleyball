c4eeef0fe3d99809fcb5bb2d27d4566758ece820
Lee, Kyutae <gorisanson@gmail.com>
2022-06-04 12:05:23 +0900

    Adjust settings.RESOLUTION in replay_player.js
    to be set to ceied value.

--- src/resources/js/replay/replay_player.js
+++ src/resources/js/replay/replay_player.js
@@ -38,7 +38,7 @@ class ReplayPlayer {
     CanvasRenderer.registerPlugin('prepare', CanvasPrepare);
     CanvasRenderer.registerPlugin('sprite', CanvasSpriteRenderer);
     Loader.registerPlugin(SpritesheetLoader);
-    settings.RESOLUTION = window.devicePixelRatio;
+    settings.RESOLUTION = Math.ceil(window.devicePixelRatio);
     settings.SCALE_MODE = SCALE_MODES.NEAREST;
     settings.ROUND_PIXELS = true;
 
@@ -61,7 +61,7 @@ class ReplayPlayer {
 
   readFile(file) {
     // Adjust PIXI settings;
-    settings.RESOLUTION = window.devicePixelRatio;
+    settings.RESOLUTION = Math.ceil(window.devicePixelRatio);
     settings.SCALE_MODE = SCALE_MODES.NEAREST;
     settings.ROUND_PIXELS = true;
 
